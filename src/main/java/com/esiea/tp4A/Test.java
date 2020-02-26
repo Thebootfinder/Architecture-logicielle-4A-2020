b40d47dbf279ecbf32f9175a0c907be8f63926c0
LaetitiaBod <laetitiabodinier@gmail.com>
2020-02-26 16:20:29 +0100

    moves implemented

---
+++ src/main/java/com/esiea/tp4A/Test.java
@@ -0,0 +1,16 @@
+package com.esiea.tp4A;
+
+import com.esiea.tp4A.domain.Direction;
+import com.esiea.tp4A.domain.Position;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Position position = Position.of(0, 0, Direction.NORTH);
+		NotreMarsRover marsrover = new NotreMarsRover(position);
+		marsrover.move("rbbf");
+		System.out.printf(marsrover.position.getX() +" "+ marsrover.position.getY() +" "+ marsrover.position.getDirection());
+	}
+
+}
