bf9ff4b8e275bc948e63a3f73abc0eb7799f7c85
LaetitiaBod <laetitiabodinier@gmail.com>
2020-04-11 15:27:07 +0200

    update tests rover moving and positionpoint+getpos

---
+++ src/test/java/com/esiea/tp4A/RoverTest.java
@@ -0,0 +1,32 @@
+package com.esiea.tp4A;
+
+import com.esiea.tp4A.domain.Direction;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RoverTest {
+    /* 
+        TODO : 
+    
+        faire différents tests dans csvsource
+        prendre en compte que quand on est à 50 et qu'on avance, on sera à -50 quand ce sera implémenté
+    */
+    @ParameterizedTest
+    @CsvSource(value= {
+        "0,0,NORTH,frfl,1,1,NORTH"
+    })
+    void is_rover_moving_properly(int X, int Y, Direction direction, String commands, int X_expected, int Y_expected, Direction direction_expected) {
+        Moving position = init(X,Y,direction);
+        Rover rover = new Rover(position);
+        rover.move(commands);
+        assertThat(rover.getPosition().getX()).isEqualTo(X_expected);
+        assertThat(rover.getPosition().getY()).isEqualTo(Y_expected);
+        assertThat(rover.getPosition().getDirection()).isEqualTo(direction_expected);
+    }
+
+    private Moving init(int X, int Y, Direction direction) {
+        PositionPoint point = new PositionPoint(X, Y);
+        return new Moving(point, direction);
+    }
+}
