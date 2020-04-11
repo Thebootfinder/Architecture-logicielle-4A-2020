6574734a196510609c02faadf00950331313d861
LaetitiaBod <laetitiabodinier@gmail.com>
2020-04-11 18:14:25 +0200

    updating tests with Mars functionnality

--- src/test/java/com/esiea/tp4A/RoverTest.java
+++ src/test/java/com/esiea/tp4A/RoverTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RoverTest {
+
+    public final Mars mars = new Mars(100);
     /* 
         TODO : 
     
@@ -14,11 +16,11 @@ public class RoverTest {
     */
     @ParameterizedTest
     @CsvSource(value= {
-        "0,0,NORTH,frfl,1,1,NORTH"
+        "0,0,NORTH,ff,0,2,NORTH"
     })
-    void is_rover_moving_properly(int X, int Y, Direction direction, String commands, int X_expected, int Y_expected, Direction direction_expected, Mars mars) {
-        Moving position = init(X,Y,direction, mars);
-        Rover rover = new Rover(position);
+    void is_rover_moving_properly(int X, int Y, Direction direction, String commands, int X_expected, int Y_expected, Direction direction_expected) {
+        
+        Rover rover = new Rover(init(X, Y, direction, mars), mars);
         rover.move(commands);
         assertThat(rover.getPosition().getX()).isEqualTo(X_expected);
         assertThat(rover.getPosition().getY()).isEqualTo(Y_expected);
