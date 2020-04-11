a11157b02c13772991ca2abf384984744b879e85
XiaBenjamin <EdenEternal25>
2020-04-11 18:15:29 +0200

    Add Laser, Update Rover

--- src/main/java/com/esiea/tp4A/Laser.java
+++ src/main/java/com/esiea/tp4A/Laser.java
@@ -1,4 +1,55 @@
 package com.esiea.tp4A;
 
+import com.esiea.tp4A.domain.Direction;
+
 public class Laser {
+    private final Mars mars;
+    private int range;
+
+    public Laser(Mars mars, int range) {
+        this.mars = mars;
+        this.range = range;
+    }
+
+    public void shot(Direction dir, Moving pos) {
+        switch(dir) {
+            case NORTH: shotNorth(pos); break;
+            case SOUTH: shotSouth(pos); break;
+            case EAST: shotEast(pos); break;
+            case WEST: shotNorth(pos); break;
+            default: break;
+        }
+    }
+
+    private void shotNorth(Moving pos) {
+        PositionPoint point = new PositionPoint(pos.getX(), pos.getY(), mars);
+        for(int i = 0; i < this.range; i++) {
+            /* détection d'obstacle */
+            /* destruction d'obstacle si détecté */
+        }
+    }
+
+    private void shotSouth(Moving pos) {
+        PositionPoint point = new PositionPoint(pos.getX(), pos.getY(), mars);
+        for(int i = 0; i < this.range; i++) {
+            /* détection d'obstacle */
+            /* destruction d'obstacle si détecté */
+        }
+    }
+
+    private void shotEast(Moving pos) {
+        PositionPoint point = new PositionPoint(pos.getX(), pos.getY(), mars);
+        for(int i = 0; i < this.range; i++) {
+            /* détection d'obstacle */
+            /* destruction d'obstacle si détecté */
+        }
+    }
+
+    private void shotWest(Moving pos) {
+        PositionPoint point = new PositionPoint(pos.getX(), pos.getY(), mars);
+        for(int i = 0; i < this.range; i++) {
+            /* détection d'obstacle */
+            /* destruction d'obstacle si détecté */
+        }
+    }
 }
--- src/main/java/com/esiea/tp4A/Rover.java
+++ src/main/java/com/esiea/tp4A/Rover.java
@@ -5,9 +5,14 @@ import com.esiea.tp4A.domain.*;
 public class Rover implements MarsRover {
 	
 	final Moving moving;
+	final private Mars mars;
+	final private Laser laser;
 
-    public Rover(Moving moving) {
+    public Rover(Moving moving, Mars mars) {
         this.moving = moving;
+        this.mars = mars;
+        this.laser = new Laser(mars,1);
+        /* default range = 1, une fonction doit pouvoir la modifier */
     }
 
     @Override
@@ -24,6 +29,7 @@ public class Rover implements MarsRover {
             case 'r': moving.rotateRight(); break;
             case 'f': moving.goForward(); break;
             case 'b': moving.goBackward(); break;
+            case 's': laser.shot(moving.getDirection(), moving); break;
             default: System.out.println("Not a valid command. Please try again"); break;
         }
     }
