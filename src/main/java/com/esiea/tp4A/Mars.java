3ee63cf99924811cd7c8857908a99e49bf5e2f91
Bleuze <bleuze@et.esiea.fr>
2020-04-11 16:09:46 +0200

    Add Init.java

--- src/main/java/com/esiea/tp4A/Mars.java
+++ src/main/java/com/esiea/tp4A/Mars.java
@@ -1,5 +1,6 @@
 package com.esiea.tp4A;
 
+
 import java.util.Set;
 
 import com.esiea.tp4A.domain.PlanetMap;
@@ -8,11 +9,17 @@ import com.esiea.tp4A.domain.Position;
 public class Mars implements PlanetMap {
 
     private Map Coord;
+    private final int [][] map;
+    private final int SizeMap;
 
     public Mars(int SizeMap) {
         Coord = new Map(SizeMap/2, (((SizeMap/2) - 1)*-1), SizeMap/2, (((SizeMap/2) - 1)*-1));
     }
-
+    
+    public int[][] getMap() {
+        return map;
+    }
+    
     @Override
     public Set<Position> obstaclePositions() {
         // TODO Auto-generated method stub
