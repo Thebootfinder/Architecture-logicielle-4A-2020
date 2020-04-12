91a271a0f447d44e2c798de462d49b5d3761e21a
anthony <agedeon136@gmail.com>
2020-04-12 04:46:17 +0200

    adding geneObstacle and checkObstacle

--- src/main/java/com/esiea/tp4A/Laser.java
+++
@@ -1,4 +0,0 @@
-package com.esiea.tp4A;
-
-public class Laser {
-}
--- src/main/java/com/esiea/tp4A/Mars.java
+++ src/main/java/com/esiea/tp4A/Mars.java
@@ -1,7 +1,9 @@
 package com.esiea.tp4A;
 
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import com.esiea.tp4A.domain.PlanetMap;
 import com.esiea.tp4A.domain.Position;
@@ -9,6 +11,8 @@ import com.esiea.tp4A.domain.Position;
 public class Mars implements PlanetMap {
 
     private Map Coord;
+    private Set<Position> obstacles = new HashSet<>();
+    private Set<Rover> roverss = new HashSet<>();
 
     public Mars(int SizeMap) {
         Coord = new Map(SizeMap/2, (((SizeMap/2) - 1)*-1), SizeMap/2, (((SizeMap/2) - 1)*-1));
@@ -20,10 +24,40 @@ public class Mars implements PlanetMap {
         return null;
     }
 
+    public void generationObstacles(int numb){
+        if (numb >= 0) {
+           /* while(obstacles.size() !=numb){
+                Position obstacle = CreationRandomObstacle();
+                obstacles.add(obstacle);
+            }*/
+        }
+    }
+
+
+    public boolean checkIfObstacle(int x, int y) {
+        return Stream.concat(roverss.stream().map(Rover::getPosition),
+            obstacles.stream())
+            .anyMatch(p -> p.getX() == x && p.getY() == y);
+    }
+
+    public void destroyObstacle(int x, int y) {
+        obstacles.removeIf(position -> position.getX() == x && position.getY() == y);
+        roverss.removeIf(rover -> rover.getPosition().getX() == x && rover.getPosition().getY() == y);
+    }
+
+
+
     public Map getCoordinates() {
         return Coord;
     }
 
-        
+    public void setObstacles(Set<Position> obstacles) {
+        this.obstacles = obstacles;
+    }
+
+    public boolean addRov(Rover rover) {
+        return roverss.add(rover);
+    }
+
         
 }
